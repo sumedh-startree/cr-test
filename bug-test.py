07d24268daec5459747a50af6be1b34c44177cdf
sumedh-startree <sumedh.gondane@startree.ai>
2025-11-04 12:59:53 +0530

    Create bug-test.py

---
+++ bug-test.py
@@ -0,0 +1,17 @@
+import inspect
+
+def dummy(fn):
+    return fn
+
+@dummy
+def test1(a, b):
+    return a
+
+@dummy
+#comment
+def test2(a, b):
+    return a
+
+print(inspect.getsourcelines(test1))
+print("------------------")
+print(inspect.getsourcelines(test2))   
